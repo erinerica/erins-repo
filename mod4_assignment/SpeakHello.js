8a6cc984d7540259753e7720f2af693e3285bce1
erin <erinerica@Erins-MBP.attlocal.net>
2023-02-18 09:28:42 -0600

    SpeakHello round 1

--- mod4_assignment/SpeakHello.js
+++ mod4_assignment/SpeakHello.js
@@ -5,7 +5,8 @@
 // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
-// var helloSpeaker =
+(var helloSpeaker = function speak(name) {
+};
 
 // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
 var speakWord = "Hello";
@@ -14,8 +15,8 @@ var speakWord = "Hello";
 // helloSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
 function speak(name) {
-  console.log(speakWord + " " + name);
-}
+  console.log("Hello" + name);
+})();
 
 // STEP 5: Expose the 'helloSpeaker' object to the global scope. Name it
 // 'helloSpeaker' on the global scope as well.
